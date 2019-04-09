0a97ea7f7ca796119d7b6413e590312bbdeba545
diogolimamarques <diogolimamarques@hotmail.com>
2019-04-09 16:16:46 -0300

    Add files via upload

--- LEXICO/AutomatoNumeros.py
+++ LEXICO/AutomatoNumeros.py
@@ -1,58 +1,61 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr  2 2019
+Created on Tue Apr  2 12:12:18 2019
 
 @author: Antonio Raian e Diogo Lima
 """
 
-def automato_numeros(palavra):
-
+def automatoNumeros(palavra):
+    
     tamanho = len(palavra)
     i = 0
     fim = 0
-
+    
     if palavra[i] == '-':
-
+        
         i = i+1
         while fim < 1:
             if palavra[i] == ' ':
                 i = i+1
             else:
                 fim = 1
-
+    
     fim = 0
-
+    
     if palavra[i] in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
         i = i+1
-
+        
         while fim < 1:
             if palavra[i] in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
                 i = i+1
-
+                
                 if i == tamanho: """ Fim da palavra, sucesso (não possui casas decimais) """
-                    return 1
+                    return "NRO " + palavra
             else
                 fim = 1
-
+                
         fim = 0
 
         if palavra[i] == '.':
             i = i+1
-
+            if i == tamanho: """Fim da palavra, ERRO DE NUMERO"""
+                return "ERR_NRO" + palavra
+            
+            
             if palavra[i] in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
                 i = i+1
-
+                
                 while fim < 1:
                     if palavra[i] in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
                         i = i+1
-
-                        if i == tamanho:
-                            return 1 """ Fim da palavra, sucesso (possui casas decimais) """
+                    
+                        if i == tamanho: 
+                            return "NRO" + palavra """ Fim da palavra, sucesso (possui casas decimais) """
                     else:
-                        return 0 """ Erro, caractere diferente de número após a primeira casa decimal """
+                        return "ERR_NRO" + palavra """ Erro, caractere diferente de número após a primeira casa decimal """
             else:
-                return 0 """ Erro, caractere diferente de número após o ponto """
+                return "ERRO DE IMPLEMENTAÇÃO DO SCANNER (Passou lexema sem usar . como delimitador" + palavra """ Erro, caractere diferente de número após o ponto """
         else:
-            return 0 """ Erro, algo diferente de um ponto depois do último número inteiro """
+            return "ERR_NRO" + palavra """ Erro, algo diferente de um ponto depois do último número inteiro """
     else:
-        return 0 """ Erro, palavra inicia com algo diferente de '-' ou número """
+        return 0 """ Erro, autômato não consumiu nada """
